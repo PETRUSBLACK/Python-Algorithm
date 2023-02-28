6ede6b2d38db67b54c105302fb07cbc506d75093
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-02-28 14:12:56 +0100

    module of 5

---
+++ Exercise 6.py
@@ -0,0 +1,10 @@
+# Iterate the given list of numbers and print only those numbers 
+# which are divisible by 5
+
+def Exercise_6(x):
+    for i in x:
+        if i % 5 == 0:
+            print(i)
+
+Exercise_6([5,12,15,28,20])
+
