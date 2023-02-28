f3bcbf44d712fee584560e33207bf94d97ca4e81
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-02-28 14:19:14 +0100

    sub-string.count

---
+++ Exercise 7.py
@@ -0,0 +1,8 @@
+# Write a program to find how many times substring “Emma” appears in the given string.
+
+def Exercise_7(x):
+    if "emma" in x:
+        print(x.count("emma"))
+
+
+Exercise_7("emma is a good boy, emma is emma")
