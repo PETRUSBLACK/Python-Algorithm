b347b05df94974ea6c650c2c1e72773c6063f072
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-02-28 14:02:53 +0100

    slicing

---
+++ Exercise 4.py
@@ -0,0 +1,13 @@
+# Write a program to remove characters from a string starting from zero up to n and 
+# return a new string.
+
+# For example:
+
+# remove_chars("pynative", 4) so output must be tive. Here we need to remove first four characters from a string.
+# remove_chars("pynative", 2) so output must be native. Here we need to remove first two characters from a string.
+
+def Exercise_4(x,y):
+    print(x[y:])
+
+Exercise_4('pynative', 4)
+Exercise_4('pynative', 2)
