1e1db02afaf522af5bdf5fb87ea0036824495397
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-02 11:35:35 +0100

    iteration in interation

--- Exercise 13.py
+++ Exercise 13.py
@@ -1,17 +1,9 @@
 # Print multiplication table form 1 to 10
 
 def Exercise_13():
-    # a = [1,2,3,4,5,6,7,8,9,10]
-    # b = [1,2,3,4,5,6,7,8,9,10]
-    # c = 0
-    # d = []
-    # for i in a:
-    #     for v in b:
-    #         print(i*v)
-
     for i in range(1, 11):
         for j in range(1, 11):
-            print(i * j, end=None)
+            print(i * j, end="|")
         print("\t\t")
 
 Exercise_13()
