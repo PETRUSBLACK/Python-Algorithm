90ebc225e180eb18e4b420fd598508705311e3af
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-03 08:55:09 +0100

    accept input from users

---
+++ Input and Output/Exercise 1.py
@@ -0,0 +1,8 @@
+# Write a program to accept two numbers from the user and calculate multiplication
+
+def Exercise_1():
+    x = int(input("Enter a number \n"))
+    b = int(input("Enter a another number \n"))
+    print("The product of the two number are", x * b)
+
+Exercise_1()
