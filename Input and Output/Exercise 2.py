8febe12312891251c6f51e5d0c879f4a0e52a0d7
PETRUSBLACK <onukwupetrusoge@gmail.com>
2023-03-03 09:35:07 +0100

    adding sep and end to the string

---
+++ Input and Output/Exercise 2.py
@@ -0,0 +1,10 @@
+# Display three string “Name”, “Is”, “James” as “Name**Is**James”
+
+# Use the print() function to format the given words in the mentioned format. 
+# Display the ** separator between each string.
+
+def Exercise_2():
+    print('My', 'Name', 'Is', 'James', sep='**')
+
+
+Exercise_2()
